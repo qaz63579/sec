607a8e5b9250bebbce6dff7ea95405c79f6be982
qaz63579 <qaz63579@gmail.com>
2019-08-23 17:53:58 +0800

    user login

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/content','ContentController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/game','GameController@index');
